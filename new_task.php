e80f595a4b153f996cb5243e460037f94dc69c1f
Yixing jiang <ethanjyx@umich.edu>
2015-03-10 23:20:38 -0400

    part 2

--- new_task.php
+++ new_task.php
@@ -1,3 +1,14 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
 $data = implode(' ', array_slice($argv, 1));
 if(empty($data)) $data = "Hello World!";
 $msg = new AMQPMessage($data,
@@ -7,3 +18,8 @@ $msg = new AMQPMessage($data,
 $channel->basic_publish($msg, '', 'task_queue');
 
 echo " [x] Sent ", $data, "\n";
+
+$channel->close();
+$connection->close();
+
+?>
